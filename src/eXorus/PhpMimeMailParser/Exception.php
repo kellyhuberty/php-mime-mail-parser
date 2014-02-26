2200fa9ecae977ae3eaa7c73f736424defb004e4
Kelly Huberty <khuberty@brokerbin.com>
2014-02-26 09:57:55 -0600

    Added namespacing...

---
+++ src/eXorus/PhpMimeMailParser/Exception.php
@@ -0,0 +1,27 @@
+<?php
+namespace eXorus\PhpMimeMailParser;
+
+/**
+ * Fast Mime Mail parser Class using PHP's MailParse Extension
+ * @author gabe@fijiwebdesign.com
+ * @url http://www.fijiwebdesign.com/
+ * @license http://creativecommons.org/licenses/by-sa/3.0/us/
+ * @version r27
+ *
+ * This fork found on: https://github.com/eXorus/php-mime-mail-parser/
+ * with contributions by eXorus 
+ */
+
+/**
+ * Remove following require_once if you have PSR-0 class loader
+ * @see https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md
+ */
+
+class Exception extends \Exception
+{
+
+
+
+
+
+}
